68caf3f4227bd67ca2c8f7f7deb805383f055abd
Szymonixo <szymonixo007@gmail.com>
2025-08-10 20:27:46 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -83,11 +83,13 @@ systemctl start apache2
 
 echo -e "${INFO}Changing backgrounds..."
 
+rm /usr/share/plymouth/themes/sdatheme -r || echo "clear installation..."
+
 cd /usr/share/plymouth/themes
 sudo cp -a pix sdatheme
 cd sdatheme
-sudo mv pix.plymouth sdatheme.plymouth
-sudo mv pix.script sdatheme.script
+sudo mv "pix.plymouth" "sdatheme.plymouth"
+sudo mv "pix.script" "sdatheme.script"
 sudo rm splash.png
 cp $PIOSK_DIR/default/sda.png /usr/share/plymouth/themes/sdatheme/splash.png
 
